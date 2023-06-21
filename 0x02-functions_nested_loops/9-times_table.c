e457bb1b0a7410b186cf03dadf71d3b816fa8538
Achraf Rima <achrafrima960@gmail.com>
2023-06-21 08:10:32 -0700

    ok

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,41 +1,19 @@
 #include "main.h"
 
 
-	/**
-	 * main - check the code
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    times_table();
-	    return (0);
-	}
+/**
+* add - adds two integers and returns the result
+* @a: int to be added to b
+* @b: int to be added to a
+* Return: sum of a and b
+*/
+int add(int a, int b)
+{
+int c;
 
 
-
-=======================================
-
-
-10-add.c CODE
+c = a + b;
 
 
-#include "main.h"
-
-
-	/**
-	 * add - adds two integers and returns the result
-	 * @a: int to be added to b
-	 * @b: int to be added to a
-	 * Return: sum of a and b
-	 */
-	int add(int a, int b)
-	{
-		int c;
-
-
-		c = a + b;
-
-
-		return (c);
-	}
+return (c);
+}
